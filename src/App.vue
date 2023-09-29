f1e77ba060c74a71299c28399d8f48f8d6db88bf
KolomatskiiOleksandr <appologet@gmail.com>
2023-09-29 10:35:28 +0300

    deleted helloworld and fixed index.html

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <LanguageSelector />
-    <router-view />
+    <router-view></router-view>
     <universal-modal v-if="isModalOpen" @close="closeModal">
       <p>This is the content of the universal modal.</p>
     </universal-modal>
@@ -14,25 +14,23 @@ import LanguageSelector from './components/LanguageSelector.vue';
 
 export default {
   name: 'App',
+  components: {
+    UniversalModal,
+    LanguageSelector
+  },
   data() {
     return {
       isModalOpen: false
     };
   },
   methods: {
-    openModal() {
-      this.isModalOpen = true;
-    },
     closeModal() {
       this.isModalOpen = false;
     }
-  },
-  components: {
-    UniversalModal,
-    LanguageSelector
   }
 };
 </script>
+
 
 <style>
 /* Add your global CSS styles here */
